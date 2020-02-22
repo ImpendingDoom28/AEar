253ce4fa3f12e1e9a2aaedf99898c2563ce4f2c2
DaniilBogomolov <bogomolov.daniil.99@gmail.com>
2020-02-22 19:40:36 +0300

    add code to create dataset with musdb tracks

---
+++ Samples/DatasetPreparation/tracksCutter.py
@@ -0,0 +1,74 @@
+from math import ceil
+import os
+import librosa
+import musdb
+import numpy as np
+from scipy.io.wavfile import write
+
+
+def divide_audio_into_frames(song, frames=9) -> None:
+    """
+    Divide audio track into equal pieces and save them
+    :param song: comes as MultiTrack representation from MUSDB
+    :param frames: parts to divide track by
+    """
+    assert song is not None
+    vocals_stft = librosa.stft(librosa.to_mono(song.targets['vocals'].audio.T))
+    accompaniment_stft = librosa.stft(librosa.to_mono(song.targets['accompaniment'].audio.T))
+    song_stft = librosa.stft(librosa.to_mono(song.audio.T))
+    parts = ceil(song_stft.shape[1] / frames)
+    for i in range(parts - 1):
+        mask = binary_mask(accompaniment_stft[:, i * frames: i * frames + frames],
+                           vocals_stft[:, i * frames: i * frames + frames])
+        track_part = song_stft[:, i * frames: i * frames + frames]
+        save_track_and_its_mask(track_part, mask)
+    return
+
+
+def binary_mask(source, target):
+    """
+        :param source: Everything in the mix except for target (for example: for vocals it would be accompaniment)
+        :param target: Target source that needs to be separated
+        :return: Binary mask for separation the target from full mix
+        """
+    assert source is not None and target is not None
+    assert source.shape == target.shape
+
+    mask = np.zeros(source.shape)
+
+    iterator = np.nditer(source, flags=['multi_index'])
+
+    while not iterator.finished:
+        source_item = iterator[0]
+        target_item = target[iterator.multi_index]
+
+        if np.abs(target_item) > np.abs(source_item):
+            mask[iterator.multi_index] = 1
+
+        iterator.iternext()
+
+    return mask
+
+
+def save_track_and_its_mask(track, mask, directory_to_save='dataset'):
+    """
+    Track will be saved as wav file, mask will be saved in .npy format (which is easy to work with using np.load(filename))
+    :param track: track to save, should be ndarray data type
+    :param mask: computed mask, should by ndarray data type
+    """
+    global TRACK_NAME
+    if not os.path.exists(directory_to_save):
+        os.mkdir(directory_to_save)
+        os.mkdir(directory_to_save + '/tracks')
+        os.mkdir(directory_to_save + '/masks')
+    # print('Saving ' + str(TRACK_NAME) + 'track_file')
+    write(directory_to_save + '/tracks/' + str(TRACK_NAME) + '.wav', 44100, librosa.istft(track))
+    np.save(directory_to_save + '/masks/' + str(TRACK_NAME), mask)
+    TRACK_NAME += 1
+
+
+TRACK_NAME = 0
+
+db = musdb.DB(root='~/MUSDB18/MUSDB18-7')
+for i in range(db.__len__()):
+    divide_audio_into_frames(db[i])
