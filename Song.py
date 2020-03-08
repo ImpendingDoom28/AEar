ebf1afd8f05e8842847b5e7dd59e82c201288efd
DaniilBogomolov <bogomolov.daniil.99@gmail.com>
2020-03-08 22:18:54 +0300

    add Song class, constructed as a collection of stft parts with iterator

---
+++ Song.py
@@ -0,0 +1,31 @@
+import librosa
+
+
+class Song(object):
+
+    def __init__(self, track, part_size=25):
+        self.stft = librosa.stft(librosa.to_mono(track.audio.T))
+        self.size = part_size
+
+    def __iter__(self):
+        return Song.SongIterator(self.stft, self.size)
+
+    class SongIterator:
+
+        def __init__(self, stft, part_size):
+            self.stft = stft
+            self.index = 0
+            self.step = part_size
+            self.end_index = stft.shape[1]
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if self.index + self.step < self.end_index:
+                start_index = self.index
+                end_index = self.index + self.step
+                self.index += 1
+                return self.stft[:, start_index: end_index]
+            else:
+                raise StopIteration
